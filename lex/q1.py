8ceeb42156de073a157d3a06bf3cc1da561f0e27
Nachiket Bhat <nachiket.bhatt@mail.utoronto.ca>
2020-11-13 23:31:15 -0500

    needs debugging, not showing proper results

--- lex/q1.py
+++ lex/q1.py
@@ -57,6 +57,7 @@ def lesk(sentence: Sequence[WSDToken], word_index: int) -> Synset:
     best_sense = mfs(sentence, word_index)
     best_score = 0
     context = set([wsd.wordform for wsd in sentence])
+    print(context)
     for synset in wn.synsets(sentence[word_index].lemma):
         signature = set()
         definition = synset.definition()
@@ -65,9 +66,6 @@ def lesk(sentence: Sequence[WSDToken], word_index: int) -> Synset:
         for example in examples:
             signature.union(set(stop_tokenize(example)))
         score = len(context.intersection(signature))
-        print(synset)
-        print(score)
-        print()
         if score > best_score:
             best_score = score
             best_sense = synset
